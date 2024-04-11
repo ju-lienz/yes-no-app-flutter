cb0db267cea2d6801cc03071c7d3d6bd259988ea
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-04-11 10:58:08 -0300

    feat: Create domain folder with entities/message.dart for chat message model

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,10 @@
+/* identifies who is the sender of the message */
+enum FromWho { mine, his }
+
+class Message {
+  final String text;
+  final String? imageURL;
+  final FromWho fromWho;
+
+  Message({required this.text, this.imageURL, required this.fromWho});
+}
