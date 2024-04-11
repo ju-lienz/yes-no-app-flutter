38755248d04d505236197984d73211252d739fe9
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-04-11 08:23:04 -0300

    refactor: optimize code for messageFieldBox widget

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,18 +5,27 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-    return TextFormField(
-      decoration: InputDecoration(
-        filled: true,
-        enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: colors.primary),
-            borderRadius: BorderRadius.circular(20)),
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send),
-          onPressed: () {},
-        ),
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send),
+        onPressed: () {},
       ),
+    );
+    return TextFormField(
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print("Submit value $value");
+      },
+      onChanged: (value) {
+        print("Changed: $value");
+      },
     );
   }
 }
