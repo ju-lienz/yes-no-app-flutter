5b47041d250be6ab114e660155c47b39d2dee51b
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-04-16 08:04:54 -0300

    feat: add get_yes_no.dart for HTTP request, import dio package, and update chat_provider.dart for API response functionality

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 /* ChatProvider can notify when there are changes */
 class ChatProvider extends ChangeNotifier {
   /* Variable that keep a ScrollController instance */
   final ScrollController chatScrollController = ScrollController();
+
+  /* Variable that keep a GetYesNoAnswer instance */
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: "Hola, buen dia!", fromWho: FromWho.me),
@@ -13,15 +17,23 @@ class ChatProvider extends ChangeNotifier {
 
   /* Methods */
   Future<void> sendMessage(String text) async {
-    /* If the message is empty, left the function */
-    if (text.isEmpty) return;
+    if (text.isEmpty) return; // If the message is empty, left the function
 
     final newMessage = Message(text: text, fromWho: FromWho.me);
+
     messageList.add(newMessage);
-    /* Indicates that the provider has changed and that you must notify
-    everyone who is listening to this provider.*/
+
+    if (text.endsWith('?')) {
+      await herReply();
+    }
+
     notifyListeners();
     moveScrollToBottom();
+  }
+
+  /* Function will be executed when it detects that a question has been sent */
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
   }
 
   Future<void> moveScrollToBottom() async {
